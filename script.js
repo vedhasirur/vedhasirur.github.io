7bd88c6b055a5fa6bcdd57a7bcc7be50d9885df2
vedhasirur <79853394+vedhasirur@users.noreply.github.com>
2024-10-09 22:46:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let currentImageIndex = 0;
+const images = [
+    "https://via.placeholder.com/400x250?text=Project+Image+1",
+    "https://via.placeholder.com/400x250?text=Project+Image+2",
+    "https://via.placeholder.com/400x250?text=Project+Image+3"
+];
+
+function nextSlide() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    document.getElementById('carousel-img').src = images[currentImageIndex];
+}
+
+function prevSlide() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    document.getElementById('carousel-img').src = images[currentImageIndex];
+}
 
